f0d87e8766a2f65559718c1ece1c2ae8706338c3
0xNineteen <0x39015319@gmail.com>
2022-10-19 08:59:08 -0400

    update for ix/logs + vault balance

--- update_config.py
+++ update_config.py
@@ -1,4 +1,3 @@
-#%%
 import json 
 from solana.publickey import PublicKey
 with open(f'../driftpy/protocol-v2/programs/clearing_house/src/lib.rs', 'r') as f:
@@ -12,7 +11,24 @@ print('program pk:', pk)
 with open("config.json", 'r') as f: 
     data = json.load(f) 
 
-data['accounts_selector'] = {'owners': [str(pk)]}
+# watch vault account balances 
+import sys
+sys.path.insert(0, '../driftpy/src/')
+from driftpy.addresses import * 
+
+accounts = []
+n_spot_markets = 1
+for i in range(n_spot_markets):
+    spot_vault_public_key = get_spot_market_vault_public_key(pk, i)
+    insurance_vault_public_key = get_insurance_fund_public_key(pk, i)
+
+    accounts.append(str(spot_vault_public_key))
+    accounts.append(str(insurance_vault_public_key))
+
+data['accounts_selector'] = {
+    'owners': [str(pk)],
+    'accounts' : accounts
+}
 
 with open("config.json", 'w') as f: 
     json.dump(data, f, indent=4)
